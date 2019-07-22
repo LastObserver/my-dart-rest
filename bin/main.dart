0ccdc9fac2361e9c02dd50b6b443c5b84488da0a
veshkin <veshkin@action-media.ru>
2019-07-22 09:58:20 +0500

    Dart app inited

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart_rest/my_dart_rest.dart' as my_dart_rest;
+
+main(List<String> arguments) {
+  print('Hello world: ${my_dart_rest.calculate()}!');
+}
---
+++ test/my_dart_rest_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_dart_rest/my_dart_rest.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
